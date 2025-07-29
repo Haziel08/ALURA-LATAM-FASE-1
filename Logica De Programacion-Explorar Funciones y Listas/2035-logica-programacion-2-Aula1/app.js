9afa3f08f56cd08678583ac18e74321613be07e5
Haziel <haziel635@gmail.com>
2025-07-29 10:35:42 -0600

    Juego actualizado, ayuda al jugador, reiniciar juego

--- Logica De Programacion-Explorar Funciones y Listas/2035-logica-programacion-2-Aula1/app.js
+++ Logica De Programacion-Explorar Funciones y Listas/2035-logica-programacion-2-Aula1/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
 
 function asignarTextoElemento(elemento, texto){
     let titulo = document.querySelector(elemento);
@@ -8,12 +9,25 @@ function asignarTextoElemento(elemento, texto){
 
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    console.log(typeof(numeroDeUsuario));
-    console.log(typeof(numeroSecreto));
-    console.log(numeroSecreto);
-    console.log(numeroDeUsuario)
-    console.log(numeroDeUsuario == numeroSecreto);
+
+    if(numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'intento' : 'intentos'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        //El usuario no acertó.
+        if(numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p','El número secreto es menor');
+        }else{
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
     return;
+};
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
 };
 
 function generarNumeroSecreto() {
@@ -21,5 +35,22 @@ function generarNumeroSecreto() {
     return numeroSecreto;
 };
 
-asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Indica un número del 1 al 10');
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', 'Indica un número del 1 al 10');
+    //Generar el número aleatorio
+    numeroSecreto = generarNumeroSecreto();
+    //Inicializar el número de intentos
+    intentos = 1;
+};
+
+function reiniciarJuego(){
+    //Limpiar caja
+    limpiarCaja();
+    //Indicar condiciones iniciales
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled', true);
+};
+
+condicionesIniciales();
